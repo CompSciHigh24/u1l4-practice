450d1a73439e2e3b6a86582d8cbbdb5cb9722168
CompSciHigh24 <csh-github@compscihigh.org>
2025-09-10 15:18:06 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,56 @@
+// ARRAYS
+// Task 1
+// Create an array called readingList and with at least 2 books you want to read
+
+// Add a new book to readingList using an array method 
+
+// Task 2
+// Update the second book to be "Beginning NodeJS"
+
+// Delete the first book of the array
+
+// Commit changes this section with an appropriate commit message
+// -----------------
+
+// OBJECTS
+// Task 3: Create a book object that has the keys title, author, and pages. Use values that make sense. 
+
+
+// Task 4
+// Add a key hasRead to the book object and set it to false
+
+// Console log object
+
+// Console log the hasRead property
+
+
+// Commit changes this section with an appropriate commit message
+// -----------------
+
+
+// NESTED ARRAYS & OBJECTS
+// Task 5
+// Create an empty array called teachers
+
+// Create an object for mrChambers and mrCastro that has age, height, and funFact
+
+// Add both to the teachers array
+
+// Modify the teachers array to make Mr. Chambers's height 7ft
+
+// Delete the first teacher in the array
+
+// Task 6
+let seatingChart = [
+    ["Lucas", "PJ", "Ahmed", "Jeremy"],
+    ["Kokou", "Maria", "Michael", "Scarlet"]
+]
+
+// Create an array with 4 students names and add it to the end of the seatingChart array
+
+// Modify "Michael" to be "Michael V." through the seatingChart array
+
+// Remove "Maria" from the seatingChart array
+
+// Commit changes this section with an appropriate commit message
+// Push commits to submit
